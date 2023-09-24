76ce720fab12e9dc6dad38622f1c21a0b10fa7f8
Vincent Fries <91223304+V-Fries@users.noreply.github.com>
2023-09-24 03:47:53 +0200

    Refactored src/parsing

--- src/mlx_wrapper/get_screen_size.c
+++ src/mlx_wrapper/get_screen_size.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_screen_size.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdameros <tdameros@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 03:07:18 by tdameros          #+#    #+#             */
-/*   Updated: 2023/09/24 03:07:19 by tdameros         ###   ########lyon.fr   */
+/*   Updated: 2023/09/24 03:43:46 by vfries           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ void	get_screen_size(t_engine *engine)
 	 // TODO check that screen size is not too small
 	if (X_SCREEN_SIZE <= 0 || Y_SCREEN_SIZE <= 0)
 	{
-		mlx_get_screen_size(engine->window.mlx, &engine->window.size.x, &engine->window.size.y);
+		mlx_get_screen_size(engine->window.mlx, &engine->window.size.x,
+			&engine->window.size.y);
 		engine->window.size.y -= 70;
 	}
 	if (X_SCREEN_SIZE > 0)
